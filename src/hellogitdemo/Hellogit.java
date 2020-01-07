5297bd3db9308e9ca4f49c26646261c00b05acaa
Hasee <Hasee@DESKTOP-99BIG5Q>
2020-01-07 22:32:11 +0800

    test2

--- src/hellogitdemo/Hellogit.java
+++ src/hellogitdemo/Hellogit.java
@@ -3,7 +3,7 @@ package hellogitdemo;
 public class Hellogit {
 
 	public static void main(String[] args) {
-		System.out.println("hello mygit");
+		System.out.println("hello mygit,Ëæ±ã¸Ä¸Ä");
 	}
 
 }
